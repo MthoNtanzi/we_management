400a8a5eeddc8f2a77ddf0b4266dbb383712e4cb
MthoNtanzi <mthontanzi99@gmail.com>
2025-07-01 12:01:04 +0200

    Render footer to wordpress format

--- index.php
+++ index.php
@@ -373,54 +373,9 @@
     <!-- End Frequently Asked Questions -->
 
     <!-- Footer Start -->
-    <div class="container-fluid bg-dark text-white-50 footer pt-5  wow fadeIn" data-wow-delay="0.1s">
-        <div class="container py-5">
-            <div class="row g-5">
-                <div class="col-lg-4 row-md">
-                    <h5 class="text-white mb-4">Get In Touch</h5>
-                    <p class="mb-2"><i class="fa fa-map-marker-alt me-3"></i>1031 Ives Dairy Rd</p>
-                    <p class="mb-2"><i class="fa fa-phone-alt me-3"></i>786-906-2928</p>
-                    <p class="mb-2"><i class="fa fa-envelope me-3"></i>support@wemanagecre.com</p>
-                    <div class="d-flex pt-2">
-                        <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-facebook-f"></i></a>
-                        <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-youtube"></i></a>
-                        <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-linkedin-in"></i></a>
-                    </div>
-                </div>
-                <div class="col-lg-4 row-md">
-                    <h5 class="text-white mb-4">Quick Links</h5>
-                    <a class="btn btn-link text-white-50" href="./about.html">About Us</a>
-                    <a class="btn btn-link text-white-50" href="./contact.html">Contact Us</a>
-                    <a class="btn btn-link text-white-50" href="./services.html">Our Services</a>
-                    <a class="btn btn-link text-white-50" href="blog.html">Our Blog</a>
-                </div>
-                <div class="col-lg-4 row-md">
-                    <h5 class="text-white mb-4">Business Hours</h5>
-                    <p>
-                        <i class="fa fa-clock me-3"></i>Monday - Friday: 9:00 AM - 6:00 PM <br>
-                        <i class="fa fa-clock me-3"></i>Saturday: 09:00 AM - 12:00 PM <br>
-                    </p>
-
-                </div>
-            </div>
-        </div>
-        <div class="container">
-            <div class="copyright">
-                <div class="row">
-                    <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
-                        &copy; <a class="border-bottom" href="#">WE Management</a>, All Right Reserved.
-                        Designed By <a class="border-bottom" href="https://talentmatchafrica.com">TMA</a>
-                    </div>
-                    <div class="col-md-6 text-center text-md-end">
-                        <div class="footer-menu">
-                            <a href="">Home</a>
-                            <a href="">FAQs</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <?php 
+        get_footer();
+    ?>
     <!-- Footer End -->
 
 
