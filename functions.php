cb4b2ca2374acdd130b76979671e1aa9d38eb5c0
samukelok <samkelokay2@gmail.com>
2025-07-07 16:17:45 +0200

    Make the page titles for each page dynamic

--- functions.php
+++ functions.php
@@ -267,3 +267,6 @@ function register_my_menus() {
     register_nav_menu('main-menu', __('Main Menu'));
 }
 add_action('after_setup_theme', 'register_my_menus');
+
+add_theme_support('title-tag');
+
--- template-parts/section-header.php
+++ template-parts/section-header.php
@@ -4,7 +4,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Services - We Management Group</title>
 
     <?php
     wp_head();
